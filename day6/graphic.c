5ec4bb25ea5b8d0639d69bccca074bd7711c2f8c
sk-mk2 <ee57042@meiji.ac.jp>
2018-11-16 12:41:37 +0900

    6日目a

--- day6/graphic.c
+++ day6/graphic.c
@@ -16,7 +16,7 @@ void boxfill8(unsigned char *vram,
               int y0,
               int x1,
               int y1);
-void init_screen(char *vram, int x, int y);
+void init_screen8(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram,
                    int xsize,
@@ -45,59 +45,6 @@ void putblock8_8(char *vram,int vxsize,int pxsize,int pysize,int px0,int py0,cha
 #define COL8_008484		14
 #define COL8_848484		15
 
-
-struct BootInfo {
-  char cyls, leds, vmode, reserve;    // 1 byte
-  short scrnx, scrny;                 // 2 byte
-  char *vram;                         // 4 byte
-};
-
-struct SegmentDescriptor {            // 8 byte
-  short limit_low, base_low;          // 2 byte * 2
-  char base_mid, access_right;        // 1 byte * 2
-  char limit_high, base_high;         // 1 byte * 2
-};
-
-struct GateDescriptor {               // 10 byte   
-  short offset_low, selector;         // 2 byte * 2
-	char dw_count, access_right;        // 1 byte * 2
-	short offset_high;                  // 2 byte * 2
-};
-
-void set_segmdesc(struct SegmentDescriptor * sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GateDescriptor * gd, int offset, int selector, int ar);
-void init_gdtidt(void);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-
-void HariMain(void)
-{
-    struct BootInfo *binfo = (struct BootInfo *) 0x0ff0;
-    extern char hankaku[4096];
-    char s[40], mcursor[256];
-    int mx, my;
-
-    init_gdtidt();
-    init_palette();
-    init_screen(binfo->vram,binfo->scrnx, binfo->scrny);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "ABC 123");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS");
-
-    mx = (binfo->scrnx -16) / 2;
-    my = (binfo->scrny - 28 - 16) / 2;
-
-    init_mouse_cursor8(mcursor, COL8_008484);
-    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
-
-    sprintf(s, "scrnx = %d", binfo->scrnx);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
-
-
-    for(;;){
-        io_hlt();
-    }
-}
 
 void init_palette(void)
 {
@@ -157,7 +104,7 @@ void boxfill8(unsigned char *vram,
 }
 
 
-void init_screen(char *vram, int x, int y)
+void init_screen8(char *vram, int x, int y)
 {
 	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
 	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
@@ -276,66 +223,4 @@ void putblock8_8(char *vram,
     }
   }
   return;
-}
-
-void init_gdtidt(void)
-  // global segmentation descriptor table
-  // interupt descriptor table
-{
-  // GDT : 0x00270000-0x0027ffff (65535 byte)
-  struct SegmentDescriptor * gdt = (struct SegmentDescriptor *) 0x00270000;   // 0x00270000-0x00270009 (8 byte)
-  // GateDescriptor : 0x0026f800-0x0026ffff (28672 byte)
-  struct GateDescriptor * idt = (struct GateDescriptor *) 0x0026f800;         // 0x0026f800-0x0026f807 (10 byte)
-  int i;
-
-  // initialize GDT
-  for (i=0; i < 8192; i++){
-    set_segmdesc(gdt + i, 0, 0, 0);
-  }
-  set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
-  set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
-  load_gdtr(0xffff, 0x00270000);
-
-  // initialize IDT
-  for (i=0; i < 256; i++){
-    set_gatedesc(idt + i, 0, 0, 0);
-  }
-  load_idtr(0x7ff, 0x0026f800);
-
-  return;
-}
-
-
-void set_segmdesc(struct SegmentDescriptor * sd,  // 4 byte
-                  unsigned int limit,             // 4 byte
-                  int base,                       // 4 byte
-                  int ar                          // 4 byte
-    )
-{
-  if (limit > 0xffff){
-    ar |= 0x8000;       // G_bit = 1
-    limit /= 0x1000;
-  }
-
-  sd->limit_low = limit & 0xffff;
-  sd->base_low = base & 0xffff;
-  sd->base_mid = (base >> 16) & 0xff;
-  sd->access_right = ar & 0xff;
-  sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
-	sd->base_high = (base >> 24) & 0xff;
-	return;
-}
-
-
-void set_gatedesc(struct GateDescriptor * gd,
-                  int offset,
-                  int selector,
-                  int ar)
-{
-	gd->offset_low = offset & 0xffff;
-	gd->selector = selector;
-	gd->dw_count = (ar >> 8) & 0xff;
-	gd->access_right = ar & 0xff;
-	gd->offset_high = (offset >> 16) & 0xffff;
-	return;
 }
