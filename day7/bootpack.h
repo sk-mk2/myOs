556b30eb73a8a2e61ffd9011d4eb24d2808ea7f6
sk-mk2 <ee57042@meiji.ac.jp>
2018-11-19 20:38:42 +0900

    キーボードから値取得

--- day7/bootpack.h
+++ day7/bootpack.h
@@ -21,6 +21,7 @@ struct BootInfo {   // 1x5+2x2 = 9 byte
 void io_hlt(void);
 void io_cli(void);
 void io_sti(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int  io_load_eflags(void);
 void io_store_eflags(int eflags);
